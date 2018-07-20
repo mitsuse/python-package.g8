cf873bd89bfacffea637b6590e285f360f6fec42
Tomoya Kose <tomoya@mitsuse.jp>
2018-07-21 00:40:43 +0900

    Initialize the repository.

---
+++ src/main/g8/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='$name$',
+    version='$version$',
+    description='$desc$',
+    url='$url$',
+    author='$author$',
+    author_email='$author_email$',
+    install_requires=[
+        'typing-extensions>=3.6.2<=4.0.0',
+    ],
+    license='$license$',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: $python_version$',
+    ],
+    keywords=[
+    ],
+    packages=find_packages(exclude=[
+        'tests',
+    ]),
+    entry_points={
+        'console_scripts': [],
+    },
+)
